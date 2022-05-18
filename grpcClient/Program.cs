3697f39b7791abb3db78969d3cf4b54c429b43ed
Glareone <enlarius@gmail.com>
2022-05-18 22:25:57 +0300

    proto updated, client has little updates for testing

--- grpcClient/Program.cs
+++ grpcClient/Program.cs
@@ -10,12 +10,21 @@ namespace grpcClient
     {
         static async Task Main(string[] args)
         {
-            // The port number(5001) must match the port of the gRPC server.
+            //The port number(5001) must match the port of the gRPC server.
             using var channel = GrpcChannel.ForAddress("https://localhost:5001");
-            var client = new Greeter.GreeterClient(channel);
-            var reply = await client.SayHelloAsync(
-                new HelloRequest { Name = "GreeterClient" });
-            Console.WriteLine("Greeting: " + reply.Message);
+            var client = new HistoricalFeed.HistoricalFeedClient(channel);
+            var reply = await client.SubscribeAsync(
+                new SubscribeRequest { Currency = "USD", Exchange = ExchangeType.Btc });
+            Console.WriteLine("===========================================");
+            Console.WriteLine("TimeStamp: " + reply.Timestamp);
+            Console.WriteLine("New Response:");
+            Console.WriteLine("Close: " + reply.Close);
+            Console.WriteLine("High: " + reply.High);
+            Console.WriteLine("Low: " + reply.Low);
+            Console.WriteLine("Open: " + reply.Open);
+            Console.WriteLine("VolumeBtc: " + reply.VolumeBtc);
+            Console.WriteLine("VolumeCur: " + reply.VolumeCur);
+            Console.WriteLine("WeightedPrice: " + reply.WeightedPrice);
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
