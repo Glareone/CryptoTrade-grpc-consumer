ef64a44adeca1b0fb3b581cf9748a5b6e031d12b
Glareone <enlarius@gmail.com>
2022-05-19 11:36:18 +0300

    stream data

--- grpcClient/Program.cs
+++ grpcClient/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
+using Grpc.Core;
 using Grpc.Net.Client;
 using GrpcClient;
 
@@ -13,19 +14,33 @@ namespace grpcClient
             //The port number(5001) must match the port of the gRPC server.
             using var channel = GrpcChannel.ForAddress("https://localhost:5001");
             var client = new HistoricalFeed.HistoricalFeedClient(channel);
-            var reply = await client.SubscribeAsync(
-                new SubscribeRequest { Currency = "USD", Exchange = ExchangeType.Btc });
-            Console.WriteLine("===========================================");
-            Console.WriteLine("TimeStamp: " + reply.Timestamp);
-            Console.WriteLine("New Response:");
-            Console.WriteLine("Close: " + reply.Close);
-            Console.WriteLine("High: " + reply.High);
-            Console.WriteLine("Low: " + reply.Low);
-            Console.WriteLine("Open: " + reply.Open);
-            Console.WriteLine("VolumeBtc: " + reply.VolumeBtc);
-            Console.WriteLine("VolumeCur: " + reply.VolumeCur);
-            Console.WriteLine("WeightedPrice: " + reply.WeightedPrice);
-            Console.WriteLine("Press any key to exit...");
+
+            var cts = new CancellationTokenSource(TimeSpan.FromSeconds(30));
+            using var streamingCall =
+                client.Subscribe(new SubscribeRequest {Currency = "USD", Exchange = ExchangeType.Coinbase});
+
+            try
+            {
+                await foreach (var reply in streamingCall.ResponseStream.ReadAllAsync(cts.Token))
+                {
+                    Console.WriteLine("===========================================");
+                    Console.WriteLine("TimeStamp: " + reply.Timestamp);
+                    Console.WriteLine("New Response:");
+                    Console.WriteLine("Close: " + reply.Close);
+                    Console.WriteLine("High: " + reply.High);
+                    Console.WriteLine("Low: " + reply.Low);
+                    Console.WriteLine("Open: " + reply.Open);
+                    Console.WriteLine("VolumeBtc: " + reply.VolumeBtc);
+                    Console.WriteLine("VolumeCur: " + reply.VolumeCur);
+                    Console.WriteLine("WeightedPrice: " + reply.WeightedPrice);
+                    Console.WriteLine("Press any key to exit...");
+                }
+            }
+            catch (RpcException ex) when (ex.StatusCode == StatusCode.Cancelled)
+            {
+                Console.WriteLine("Stream cancelled.");
+            }
+
             Console.ReadKey();
         }
     }
